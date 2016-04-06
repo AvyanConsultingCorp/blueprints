bff7f3f9f3a44131fa601e3e428afee9ea509281
Hanz Zhang <hanz@microsoft.com>
2016-04-06 11:32:12 -0700

    update hybrid-vpn-multi-workload-windows.cmd

--- hybrid-vpn-multi-workload-windows/hybrid-vpn-multi-workload-windows.cmd
+++ hybrid-vpn-multi-workload-windows/hybrid-vpn-multi-workload-windows.cmd
@@ -1,5 +1,3 @@
-
-
 @ECHO OFF
 SETLOCAL
 
@@ -16,23 +14,121 @@ SET SUBSCRIPTION=%1
 :: Set global avariables that is common to all virtual networks
 SET ENVIRONMENT=dev
 SET VPN_GATEWAY_TYPE=RouteBased
+SET HUB_NAME=hub0
 
-:: Create hub and spoke vnets.
-:: If you don't specify ilb_fe_ip, the internal load balancer will not 
-:: be created, which could be suited for the hub vnet.
-::
-::::::CREATE_VNET name vnet_addrss  gateway_addrss subnt_adrss  loc   ilb_fe_ip
-CALL :CREATE_VNET App0 10.0.0.0/16 10.0.255.240/28 10.0.0.0/17 eastus 
-CALL :CREATE_VNET App1 10.1.0.0/16 10.1.255.240/28 10.1.0.0/17 eastus 10.1.127.254 
-CALL :CREATE_VNET App2 10.2.0.0/16 10.2.255.240/28 10.2.0.0/17 eastus 10.2.127.254 
-CALL :CREATE_VNET App3 10.3.0.0/16 10.3.255.240/28 10.3.0.0/17 eastus 10.3.127.254 
+:::                   vnet_addrss  gateway_addrss subnt_adrss  loc    
+CALL :CREATE_HUB_VNET 10.0.0.0/16 10.0.255.240/28 10.0.0.0/17 eastus 
 
+::                       app vnet_addrss  gateway_addrss subnt_adrss  loc    ilb_fe_ip
+CALL :CREATE_SPOKE_VNET app1 10.1.0.0/16 10.1.255.240/28 10.1.0.0/17 eastus 10.1.127.254 
+CALL :CREATE_SPOKE_VNET app2 10.2.0.0/16 10.2.255.240/28 10.2.0.0/17 eastus 10.2.127.254 
+CALL :CREATE_SPOKE_VNET app3 10.3.0.0/16 10.3.255.240/28 10.3.0.0/17 eastus 10.3.127.254 
+
+GOTO :eof
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:CREATE_VNET
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:CREATE_HUB_VNET
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+IF "%~4"=="" (
+    ECHO Usage: %0 vnet_addrss gateway_addrss subnt_adrss loc    
+    ECHO   For example: %0 10.0.0.0/16 10.0.255.240/28 10.0.0.0/17 eastus 
+    EXIT /B
+    )
+
+:: Set up variables to build out the naming conventions for deployment
+SET APP_NAME=%HUB_NAME%
+SET VNET_IP_RANGE=%1
+SET GATEWAY_SUBNET_IP_RANGE=%2
+SET INTERNAL_SUBNET_IP_RANGE=%3
+SET LOCATION=%4
+
+:: Set up the names of things using recommended conventions
+SET TARGET_RESOURCE_GROUP=%APP_NAME%-%ENVIRONMENT%-rg
+SET TARGET_LOCAL_GATEWAY=%APP_NAME%-to-on-prem-lgw
+
+:: Set up the names of things using recommended conventions
+SET RESOURCE_GROUP=%APP_NAME%-%ENVIRONMENT%-rg
+SET VNET_NAME=%APP_NAME%-vnet
+SET PUBLIC_IP_NAME=%APP_NAME%-pip
+
+SET INTERNAL_SUBNET_NAME=%APP_NAME%-internal-subnet
+SET VPN_GATEWAY_NAME=%APP_NAME%-vgw
+SET VPN_CONNECTION_NAME=%APP_NAME%-vpn
+
+:: Set up the postfix variables attached to most CLI commands
+SET POSTFIX=--resource-group %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
+
+CALL azure config mode arm
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create resources
+
+:: Create the enclosing resource group
+CALL :CallCLI azure group create --name %RESOURCE_GROUP% --location %LOCATION% ^
+  --subscription %SUBSCRIPTION%
+
+:: Create the VNet
+CALL :CallCLI azure network vnet create --address-prefixes %VNET_IP_RANGE% ^
+  --name %VNET_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create the GatewaySubnet
+CALL :CallCLI azure network vnet subnet create --vnet-name %VNET_NAME% ^
+  --address-prefix %GATEWAY_SUBNET_IP_RANGE% --name GatewaySubnet %POSTFIX%
+
+:: Create public IP address for VPN Gateway
+:: Note that the Azure VPN Gateway only supports dynamic IP addresses
+CALL :CallCLI azure network public-ip create --allocation-method Dynamic ^
+  --name %PUBLIC_IP_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create virtual network gateway
+CALL :CallCLI azure network vpn-gateway create --name %VPN_GATEWAY_NAME% ^
+  --type %VPN_GATEWAY_TYPE% --public-ip-name %PUBLIC_IP_NAME% --vnet-name %VNET_NAME% ^
+  --location %LOCATION% %POSTFIX
+
+:: Parse public-ip json to get the line that contains an ip address. 
+:: There is only one line that consists the ip address
+FOR /F "delims=" %%a in ('
+    CALL azure network public-ip show -g %RESOURCE_GROUP% -n %PUBLIC_IP_NAME% --json ^| 
+    FINDSTR /R "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"
+    ') DO @SET JSON-IP-ADDRESS-LINE=%%a
+
+:: Remove the first 16 and last two charactors to get the ip address
+SET VPN-GATEWAY-PIP-ADDRESS=%JSON-IP-ADDRESS-LINE:~16,-2%
+
+:: Create local gateway
+CALL :CallCLI azure network local-gateway create --name %LOCAL_GATEWAY_NAME% ^
+  --address-space %VNET_IP_RANGE% --ip-address %VPN-GATEWAY-PIP-ADDRESS% ^
+  --location %LOCATION% %POSTFIX%
+
+:: Create a vpn connection to from this vnet to the TARGET_LOCAL_GATEWAY
+CALL :CallCLI azure network vpn-connection create --name %VPN_CONNECTION_NAME% ^
+  --vnet-gateway1 %VPN_GATEWAY_NAME% --vnet-gateway1-group %RESOURCE_GROUP% ^
+  --lnet-gateway2 %TARGET_LOCAL_GATEWAY% ^
+  --lnet-gateway2-group %TARGET_RESOURCE_GROUP% ^
+  --type IPsec --location %LOCATION% %POSTFIX%
+
+:: Create the internal subnet
+CALL :CallCLI azure network vnet subnet create --vnet-name %VNET_NAME% ^
+  --address-prefix %INTERNAL_SUBNET_IP_RANGE% --name %INTERNAL_SUBNET_NAME% %POSTFIX%
+
+GOTO :eof
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:CREATE_SPOKE_VNET
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 IF "%~6"=="" (
-    ECHO Usage: %0 app vnet_ip_range gateway_ip_range internal_subnet_ip_range location is_hub ilb_fe_ip
-    ECHO   For example: %0 App1 10.1.0.0/16 10.1.255.240/28 10.1.0.0/17 eastus false 10.1.127.254
+    ECHO Usage: %0 app vnet_addrss gateway_addrss subnt_adrss loc connect_to_lgw ilb_fe_ip
+    ECHO   For example: %0 app1 app1 10.1.0.0/16 10.1.255.240/28 10.1.0.0/17 eastus app1_to_hub_lgw 10.1.127.254
     EXIT /B
     )
 
@@ -45,13 +141,16 @@ SET LOCATION=%5
 SET INTERNAL_LOAD_BALANCER_FRONTEND_IP_ADDRESS=%6
 
 :: Set up the names of things using recommended conventions
+SET TARGET_RESOURCE_GROUP=%HUB_NAME%-%ENVIRONMENT%-rg
+SET TARGET_LOCAL_GATEWAY=%APP_NAME%-to-%HUB_NAME%-lgw
+
+:: Set up the names of things using recommended conventions
 SET RESOURCE_GROUP=%APP_NAME%-%ENVIRONMENT%-rg
 SET VNET_NAME=%APP_NAME%-vnet
 SET PUBLIC_IP_NAME=%APP_NAME%-pip
 
 SET INTERNAL_SUBNET_NAME=%APP_NAME%-internal-subnet
 SET VPN_GATEWAY_NAME=%APP_NAME%-vgw
-SET LOCAL_GATEWAY_NAME=%APP_NAME%-lgw
 SET VPN_CONNECTION_NAME=%APP_NAME%-vpn
 
 SET INTERNAL_LOAD_BALANCER_NAME=%APP_NAME%-ilb
@@ -108,19 +207,19 @@ CALL :CallCLI azure network local-gateway create --name %LOCAL_GATEWAY_NAME% ^
   --address-space %VNET_IP_RANGE% --ip-address %VPN-GATEWAY-PIP-ADDRESS% ^
   --location %LOCATION% %POSTFIX%
 
-:: Create a site-to-site connection
+:: Create a site-to-site connection to connect to the hub
 CALL :CallCLI azure network vpn-connection create --name %VPN_CONNECTION_NAME% ^
   --vnet-gateway1 %VPN_GATEWAY_NAME% --vnet-gateway1-group %RESOURCE_GROUP% ^
-  --lnet-gateway2 %LOCAL_GATEWAY_NAME% --lnet-gateway2-group %RESOURCE_GROUP% ^
+  --lnet-gateway2 %TARGET_LOCAL_GATEWAY% ^
+  --lnet-gateway2-group %TARGET_RESOURCE_GROUP% ^
   --type IPsec --location %LOCATION% %POSTFIX%
 
 :: Create the internal subnet
 CALL :CallCLI azure network vnet subnet create --vnet-name %VNET_NAME% ^
   --address-prefix %INTERNAL_SUBNET_IP_RANGE% --name %INTERNAL_SUBNET_NAME% %POSTFIX%
 
-IF %INTERNAL_LOAD_BALANCER_FRONTEND_IP_ADDRESS% == "" (
-GOTO :eof
-)
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create ILB
 
 CALL :CallCLI azure network lb create --name %INTERNAL_LOAD_BALANCER_NAME% ^
   --location %LOCATION% %POSTFIX%
@@ -146,7 +245,13 @@ CALL :CallCLI azure network lb probe create --protocol %INTERNAL_LOAD_BALANCER_P
 CALL azure network vpn-connection shared-key show --name %VPN_CONNECTION_NAME% %POSTFIX%
 
 GOTO :eof
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+
+
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :CallCLI
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -158,7 +263,13 @@ IF ERRORLEVEL 1 (
     (GOTO) 2>NULL & GOTO :eof
 )
 GOTO :eof
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+
+
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :ShowError
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
