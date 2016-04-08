0fee1ff7a4c6fccb187ddc0209fe5aa5a7aa8683
Hanz Zhang <hanz@microsoft.com>
2016-04-08 14:03:24 -0700

    update hybrid-vpn-multi-workload-windows.cmd

--- hybrid-vpn-multi-workload-windows/hybrid-vpn-multi-workload-windows.cmd
+++ hybrid-vpn-multi-workload-windows/hybrid-vpn-multi-workload-windows.cmd
@@ -5,16 +5,16 @@ IF "%~2"=="" (
     ECHO   For example: %0 xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx xxxxxxxxxxxx
     EXIT /B
     )
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Explicitly set the subscription to avoid confusion as to which subscription
 :: is active/default
 SET SUBSCRIPTION=%1
 SET IPSEC_SHARED_KEY=%2
 SET ENVIRONMENT=dev
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: create hub
+:: Create hub vnet
 SET HUB_NAME=hub0
 SET HUB_CIDR=10.0.0.0/16
 SET HUB_INTERNAL_CIDR=10.0.0.0/17
@@ -34,55 +34,16 @@ CALL :CREATE_VNET ^
     %HUB_RESOURCE_GROUP%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: HUB-ONPREM and ONPREM-HUB connections
-
-SET ONPREM_NAME=on-prem
-SET ONPREM_GATEWAY_PIP=131.107.36.3
-SET ONPREM_CIDR=192.268.0.0/24
-SET HUB_TO_ONPREM_LGW=%HUB_NAME%-to-%ONPREM_NAME%-lgw
-SET HUB_TO_ONPREM_VPN-CONNECTION=%HUB_NAME%-to-%ONPREM_NAME%-vpn-connection
-SET ONPREM_TO_HUB_LGW=%ONPREM_NAME%-to-%HUB_NAME%-lgw
-SET ONPREM_TO_HUB_TOPOLOGY_CIDR=
-
-:: HUB_TO_ONPREM_LGW
-CALL :CallCLI azure network local-gateway create ^
-  --name %HUB_TO_ONPREM_LGW% ^
-  --address-space %ONPREM_CIDR% ^
-  --ip-address %ONPREM_GATEWAY_PIP% ^
-  --location %HUB_LOCACTION% ^
-  --resource-group %HUB_RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
-
-:: HUB_TO_ONPREM_VPN-CONNECTION
-CALL :CallCLI azure network vpn-connection create ^
-  --name %HUB_TO_ONPREM_VPN-CONNECTION% ^
-  --vnet-gateway1 %HUB_GATEWAY% ^
-  --vnet-gateway1-group %HUB_RESOURCE_GROUP% ^
-  --lnet-gateway2 %HUB_TO_ONPREM_LGW% ^
-  --lnet-gateway2-group %HUB_RESOURCE_GROUP% ^
-  --type IPsec ^
-  --shared-key %IPSEC_SHARED_KEY% ^
-  --location %HUB_LOCACTION% ^
-  --resource-group %HUB_RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
-
-:: ONPREM_TO_HUB_LGW
-CALL :CallCLI azure network local-gateway create ^
-  --name %ONPREM_TO_HUB_LGW% ^
-  --address-space %ONPREM_TO_HUB_TOPOLOGY_CIDR% ^
-  --ip-address %HUB_GATEWAY_PIP% ^
-  --location %HUB_LOCACTION% ^
-  --resource-group %HUB_RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
-
-:: ONPREM_TO_HUB_VPN-CONNECTION
-:: this is created manually on premise 
-::
-::
+:: Set variable for on-prem network ONP
+SET ONP_NAME=onp
+SET ONP_GATEWAY_PIP=131.107.36.3
+SET ONP_CIDR=192.268.0.0/24
+SET ONP_LOCACTION =%HUB_LOCATION%
+SET ONP_RESOURCE_GROUP=%HUB_RESOURCE_GROPU%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: create SP1
-SET SP1_NAME=spoke1
+:: Create spoke vnet SP1 
+SET SP1_NAME=sp1
 SET SP1_CIDR=10.1.0.0/16
 SET SP1_INTERNAL_CIDR=10.1.0.0/17
 SET SP1_GATEWAY_CIDR=10.1.255.240/28
@@ -91,7 +52,6 @@ SET SP1_GATEWAY_PIP_NAME=%SP1_NAME%-vgw
 SET SP1_LOCATION=eastus
 SET SP1_RESOURCE_GROUP=%SP1_NAME%-%ENVIRONMENT%-rg
 SET SP1_ILB=10.1.127.254
-
 CALL :CREATE_VNET ^
     %SP1_NAME% ^
     %SP1_CIDR% ^
@@ -103,13 +63,89 @@ CALL :CREATE_VNET ^
     %SP1_RESOURCE_GROUP% ^
     %SP1_ILB%
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create spoke vnet SP2
+SET SP2_NAME=sp1
+SET SP2_CIDR=10.2.0.0/16
+SET SP2_INTERNAL_CIDR=10.2.0.0/17
+SET SP2_GATEWAY_CIDR=10.2.255.240/28
+SET SP2_GATEWAY_NAME=%SP2_NAME%-vgw
+SET SP2GATEWAY_PIP_NAME=%SP2_NAME%-vgw
+SET SP2_LOCATION=eastus
+SET SP2_RESOURCE_GROUP=%SP2_NAME%-%ENVIRONMENT%-rg
+SET SP2_ILB=10.2.127.254
+CALL :CREATE_VNET ^
+    %SP2_NAME% ^
+    %SP2_CIDR% ^
+    %SP2_INTERNAL_CIDR% ^
+    %SP2_GATEWAY_CIDR% ^
+    %SP2_GATEWAY_NAME% ^
+    %SP2_GATEWAY_PIP_NAME% ^
+    %SP2_LOCATION% ^
+    %SP2_RESOURCE_GROUP% ^
+    %SP2_ILB%
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create spoke vnet SP3
+SET SP3_NAME=sp3
+SET SP3_CIDR=10.3.0.0/16
+SET SP3_INTERNAL_CIDR=10.3.0.0/17
+SET SP3_GATEWAY_CIDR=10.3.255.240/28
+SET SP3_GATEWAY_NAME=%SP3_NAME%-vgw
+SET SP3GATEWAY_PIP_NAME=%SP3_NAME%-vgw
+SET SP3_LOCATION=eastus
+SET SP3_RESOURCE_GROUP=%SP3_NAME%-%ENVIRONMENT%-rg
+SET SP3_ILB=10.3.127.254
+CALL :CREATE_VNET ^
+    %SP3_NAME% ^
+    %SP3_CIDR% ^
+    %SP3_INTERNAL_CIDR% ^
+    %SP3_GATEWAY_CIDR% ^
+    %SP3_GATEWAY_NAME% ^
+    %SP3_GATEWAY_PIP_NAME% ^
+    %SP3_LOCATION% ^
+    %SP3_RESOURCE_GROUP% ^
+    %SP3_ILB%
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Create additional spokes here if necessary
+:: 
+:: 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Set gateway address space CIDR list
+:: You need enclose the CIDR list in quotes because they are comma seperated, 
+:: If without the quotes, the script subroutine will treat each CIDR as a seperate variable.
+::
+:: All the lists need to be modified if you add or remove a spoke from the topology.
+SET ONP_TO_HUB_CIDR_LIST="%HUB_CIDR%,%SP1_CIDR%,%SP2_CIDR%,%SP3_CIDR%"
+SET SP1_TO_HUB_CIDR_LIST="%HUB_CIDR%,%ONP_CIDR%,%SP2_CIDR%,%SP3_CIDR%"
+SET SP2_TO_HUB_CIDR_LIST="%HUB_CIDR%,%ONP_CIDR%,%SP1_CIDR%,%SP3_CIDR%"
+SET SP3_TO_HUB_CIDR_LIST="%HUB_CIDR%,%ONP_CIDR%,%SP1_CIDR%,%SP2_CIDR%"
+
+:: add additional SP3_TO_HUB_CIDR_LIST here if needed
+::
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: ONP connections
+CALL :CREATE_HUB_SPOKE_CONNECTION ^
+    %ONP_NAME% ^
+    %ONP_CIDR% ^
+    %ONP_TO_HUB_CIDR_LIST% ^
+    %ONP_GATEWAY_PIP% ^
+    %ONP_LOCACTION% ^
+    %ONP_RESOURCE_GROUP% ^
+    on_prem
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: SP1 connections
 CALL :CREATE_HUB_SPOKE_CONNECTION ^
     %SP1_NAME% ^
     %SP1_CIDR% ^
-    %SP1_TO_HUB_TOPOLOGY_CIDR% ^
+    %SP1_TO_HUB_CIDR_LIST% ^
     %SP1_GATEWAY_PIP_NAME% ^
     %SP1_LOCACTION% ^
-    %SP1_RESOURCE_GROUP% ^
+    %SP1_RESOURCE_GROUP%
+
+GOTO :eof
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -117,12 +153,14 @@ CALL :CREATE_HUB_SPOKE_CONNECTION ^
 :CREATE_HUB_SPOKE_CONNECTION
 
 :: input variableS
-SPK_NAME=%SP1%
-SET SPK_CIDR=%1
-SET SPK_TO_HUB_TOPOLOGY_CIDR=%2
-SET SPK_GATEWAY_PIP_NAME=%S3
-SET SPK_LOCACTION=%4
-SET SPK_RESOURCE_GROUP=%5
+SET SPK_NAME=%1
+SET SPK_CIDR=%2
+SET SPK_TO_HUB_CIDR_LIST=%3
+SET SPK_TO_HUB_CIDR_LIST=%SPK_TO_HUB_CIDR_LIST:~1,-1%
+SET SPK_GATEWAY_PIP_NAME=%4
+SET SPK_LOCACTION=%5
+SET SPK_RESOURCE_GROUP=%6
+SET ON_PREM_FLAG=%7
 
 :: azure resource names
 SET HUB_TO_SPK_LGW=%HUB_NAME%-to-%SPK_NAME%-lgw
@@ -130,15 +168,25 @@ SET HUB_TO_SPK_VPN-CONNECTION=%HUB_NAME%-to-%SPK_NAME%-vpn-connection
 SET SPK_TO_HUB_LGW=%SPK_NAME%-to-%HUB_NAME%-lgw
 SET SPK_TO_HUB_VPN-CONNECTION=%SPK_NAME%-to-%HUB_NAME%-vpn-connection
 
-:: Parse public-ip json to get the line that contains an ip address.
-:: There is only one line that consists the ip address
-FOR /F "delims=" %%a in ('
-    CALL azure network public-ip show -g %SP1_RESOURCE_GROUP% -n %SPK_GATEWAY_PIP_NAME% --json ^| 
+:::::::::::::::::::::::::::::::::::::::
+:: Retrieve SPK_GATEWAY_PIP
+
+IF "%ON_PREM_FLAG%" == "on_prem" (
+    SET SPK_GATEWAY_PIP=%SPK_GATEWAY_PIP_NAME%
+) ELSE (
+    :: Parse public-ip json to get the line that contains an ip address.
+    :: There is only one line that consists the ip address
+    FOR /F "delims=" %%a in ('
+    CALL azure network public-ip show -g %SPK_RESOURCE_GROUP% -n %SPK_GATEWAY_PIP_NAME% --json ^| 
     FINDSTR /R "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"
     ') DO @SET JSON_IP_ADDRESS_LINE=%%a
 
-:: Remove the first 16 and last two charactors to get the ip address
-SET SPK_GATEWAY_PIP=%JSON_IP_ADDRESS_LINE:~16,-2%
+    :: Remove the first 16 and last two charactors to get the ip address
+    SET SPK_GATEWAY_PIP=%JSON_IP_ADDRESS_LINE:~16,-2%
+)
+
+:::::::::::::::::::::::::::::::::::::::
+:: Retrieve HUB_GATEWAY_PIP
 
 :: Parse public-ip json to get the line that contains an ip address.
 :: There is only one line that consists the ip address
@@ -149,6 +197,9 @@ FOR /F "delims=" %%a in ('
 
 :: Remove the first 16 and last two charactors to get the ip address
 SET HUB_GATEWAY_PIP=%JSON_IP_ADDRESS_LINE:~16,-2%
+
+:::::::::::::::::::::::::::::::::::::::
+:: Create vpn gateways
 
 :: HUB_TO_SPK_LGW
 CALL :CallCLI azure network local-gateway create ^
@@ -162,11 +213,14 @@ CALL :CallCLI azure network local-gateway create ^
 :: SPK_TO_HUB_LGW
 CALL :CallCLI azure network local-gateway create ^
   --name %SP1_TO_HUB_LGW% ^
-  --address-space %SPK_TO_HUB_TOPOLOGY_CIDR% ^
+  --address-space %SPK_TO_HUB_CIDR_LIST% ^
   --ip-address %HUB_GATEWAY_PIP% ^
   --location %HUB_LOCACTION% ^
   --resource-group %HUB_RESOURCE_GROUP% ^
   --subscription %SUBSCRIPTION%
+
+:::::::::::::::::::::::::::::::::::::::
+:: Create site-to-site vpn connections
 
 :: HUB_TO_SPK_VPN-CONNECTION
 CALL :CallCLI azure network vpn-connection create ^
@@ -181,11 +235,16 @@ CALL :CallCLI azure network vpn-connection create ^
   --resource-group %HUB_RESOURCE_GROUP% ^
   --subscription %SUBSCRIPTION%
 
-:: SP1_TO_HUB_VPN-CONNECTION
-CALL :CallCLI azure network vpn-connection create ^
+:: SPK_TO_HUB_VPN-CONNECTION
+:: You do not create on-prem to hub connection in azure. 
+:: Instead, you need to go to on premise network
+:: to route the traffic to the hub gateway pip.
+
+IF NOT "%ON_PREM_FLAG%" == "on_prem" (
+  CALL :CallCLI azure network vpn-connection create ^
   --name %SPK_TO_HUB_VPN-CONNECTION% ^
-  --vnet-gateway1 %SP1_GATEWAY% ^
-  --vnet-gateway1-group %SP1_RESOURCE_GROUP% ^
+  --vnet-gateway1 %SPK_GATEWAY% ^
+  --vnet-gateway1-group %SPK_RESOURCE_GROUP% ^
   --lnet-gateway2 %SPK_TO_HUB_LGW% ^
   --lnet-gateway2-group %HUB_RESOURCE_GROUP% ^
   --type IPsec ^
@@ -193,6 +252,9 @@ CALL :CallCLI azure network vpn-connection create ^
   --location %SPK_LOCACTION% ^
   --resource-group %HUB_RESOURCE_GROUP% ^
   --subscription %SUBSCRIPTION%
+)
+
+GOTO :eof
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -212,7 +274,6 @@ SET INTERNAL_LOAD_BALANCER_FRONTEND_IP_ADDRESS=%9
 
 :: Set up the azure resource names using recommended conventions
 SET APP_SUBSCRIPTION=%SUBSCRIPTION%
-
 SET APP_VNET_NAME=%APP_NAME%-vnet
 SET APP_PUBLIC_IP_NAME=%APP_NAME%-pip
 SET APP_INTERNAL_SUBNET_NAME=%APP_NAME%-internal-subnet
@@ -275,6 +336,7 @@ CALL :CallCLI azure network vpn-gateway create ^
 
 :::::::::::::::::::::::::::::::::::::::
 :: Create ILB resources
+
 IF "%INTERNAL_LOAD_BALANCER_FRONTEND_IP_ADDRESS%"=="" (
     EXIT /B
     )
@@ -327,3 +389,44 @@ CALL azure network vpn-connection shared-key show ^
   --subscription %APP_SUBSCRIPTION%
 
 GOTO :eof
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: SUBROUTION
+:CallCLI
+
+SETLOCAL
+CALL %*
+IF ERRORLEVEL 1 (
+    CALL :ShowError "Error executing CLI Command: " %*
+    :: This executes in the CALLER'S context, so we can exit the whole script on an error
+    (GOTO) 2>NULL & GOTO :eof
+)
+GOTO :eof
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: SUBROUTION
+:ShowError
+
+SETLOCAL EnableDelayedExpansion
+
+:: Print the message
+ECHO %~1
+SHIFT
+
+:: Get the first part of the azure CLI command so we don't have an extra space at the beginning
+SET CLICommand=%~1
+SHIFT
+
+:: Loop through the rest of the parameters and recreate the CLI command
+:Loop
+    IF "%~1"=="" GOTO Continue
+    SET "CLICommand=!CLICommand! %~1"
+    SHIFT
+GOTO Loop
+:Continue
+ECHO %CLICommand%
+GOTO :eof
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
