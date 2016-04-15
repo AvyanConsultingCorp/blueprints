7af332883d48f2ed6bc6724b47a919a3c15f0c2e
Kirpa Singh <kirpas@microsoft.com>
2016-04-15 16:23:47 -0700

    Recompiled to package with a different name

--- config-tasks/IISConfig.ps1
+++ config-tasks/IISConfig.ps1
@@ -1,31 +1,4 @@
-# Configuration ConfigureWeb
-# {
-  # param ($machineName)
-
-  # Node $machineName
-  # {
-    # #Install the IIS Role
-    # WindowsFeature IIS
-    # {
-      # Ensure = "Present"
-      # Name = "Web-Server"
-    # }
-
-    # #Install ASP.NET 4.6
-    # WindowsFeature ASP
-    # {
-      # Ensure = "Present"
-      # Name = "Web-Asp-Net46"
-    # }
-
-     # WindowsFeature WebServerManagementConsole
-    # {
-        # Name = "Web-Mgmt-Console"
-        # Ensure = "Present"
-    # }
-  # }
-# }
-Configuration IIS
+Configuration ConfigureWeb
 { 
 	node ("localhost")
 	{ 
