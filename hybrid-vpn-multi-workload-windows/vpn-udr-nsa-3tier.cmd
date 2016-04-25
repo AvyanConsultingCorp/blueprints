9521039fdf9f1f77652c28c68955b5a52853a01f
Hanz Zhang <hanz@microsoft.com>
2016-04-25 16:08:18 -0700

    refactor vpn-udr-nsa-3tier.cmd

--- hybrid-vpn-multi-workload-windows/vpn-udr-nsa-3tier.cmd
+++ hybrid-vpn-multi-workload-windows/vpn-udr-nsa-3tier.cmd
@@ -1,123 +1,69 @@
 @ECHO OFF
-SETLOCAL
-
-IF "%~1"=="" (
-    ECHO Usage: %0 subscription-id
-    ECHO   For example: %0 xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+SETLOCAL EnableDelayedExpansion
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+IF "%~5"=="" (
+    ECHO Usage: %0 appname subscription-id ipsec-shared-key on-prem-gateway-pip on-prem-address-prefix
+    ECHO   For example: %0 mytest123 13ed86531-1602-4c51-a4d4-afcfc38ddad3 myipsecsharedkey123 11.22.33.44 192.168.0.0/24
     EXIT /B
     )
-
-REM Explicitly set the subscription to avoid confusion as to which subscription
-REM is active/default
-
-SET SUBSCRIPTION=%1
-
-REM Set up variables to build out the naming conventions for deployment
-
-SET APP_NAME=hybrid
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+SET APP_NAME=%1
+SET SUBSCRIPTION=%2
+SET VPN_IPSEC_SHARED_KEY=%3
+SET ON_PREMISES_PUBLIC_IP=%4
+SET ON_PREMISES_ADDRESS_SPACE=%5
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 SET LOCATION=centralus
 SET ENVIRONMENT=dev
-SET VPN_GATEWAY_TYPE=RouteBased
-
-SET VNET_IP_RANGE=10.20.0.0/16
-SET ON_PREMISES_ADDRESS_SPACE=10.10.0.0/16
-SET ON_PREMISES_PUBLIC_IP=40.50.60.70
-REM This gives the gateway an IP range 10.20.255.224 - 10.20.255.254
-SET GATEWAY_SUBNET_IP_RANGE=10.20.255.224/27
-REM This give the internal subnet an IP range of 10.20.1.1 - 10.20.1.254
-SET INTERNAL_SUBNET_IP_RANGE=10.20.1.0/24
-REM We will put this at the end of the subnet
-SET INTERNAL_LOAD_BALANCER_FRONTEND_IP_ADDRESS=10.20.1.254
-
-REM Set up the names of things using recommended conventions
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 SET RESOURCE_GROUP=%APP_NAME%-%ENVIRONMENT%-rg
-SET VNET_NAME=%APP_NAME%-vnet
-SET PUBLIC_IP_NAME=%APP_NAME%-pip
-
-SET INTERNAL_SUBNET_NAME=%APP_NAME%-internal-subnet
-SET VPN_GATEWAY_NAME=%APP_NAME%-vgw
-SET LOCAL_GATEWAY_NAME=%APP_NAME%-lgw
-SET VPN_CONNECTION_NAME=%APP_NAME%-vpn
-
-SET INTERNAL_LOAD_BALANCER_NAME=%APP_NAME%-ilb
-SET INTERNAL_LOAD_BALANCER_FRONTEND_IP_NAME=%APP_NAME%-ilb-fip
-SET INTERNAL_LOAD_BALANCER_POOL_NAME=%APP_NAME%-ilb-pool
-
-SET INTERNAL_LOAD_BALANCER_PROBE_PROTOCOL=tcp
-SET INTERNAL_LOAD_BALANCER_PROBE_INTERVAL=300
-SET INTERNAL_LOAD_BALANCER_PROBE_COUNT=4
-SET INTERNAL_LOAD_BALANCER_PROBE_NAME=%INTERNAL_LOAD_BALANCER_NAME%-probe
-
-REM Set up the postfix variables attached to most CLI commands
 SET POSTFIX=--resource-group %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+SET VNET_NAME=%APP_NAME%-vnet
+SET VNET_IP_RANGE=10.20.0.0/16
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+SET VPN_PUBLIC_IP_NAME=%APP_NAME%-pip
+SET VPN_LOCAL_GATEWAY_NAME=%APP_NAME%-lgw
+SET VPN_CONNECTION_NAME=%APP_NAME%-vpn
+SET VPN_GATEWAY_TYPE=RouteBased
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+SET NAFE_SUBNET_IP_RANGE=10.20.1.0/24
+SET NAFE_LOAD_BALANCER_FRONTEND_IP_ADDRESS=10.20.1.254
+SET NAFE_SUBNET_NAME=%APP_NAME%-nafe-subnet
+SET NAFE_LOAD_BALANCER_NAME=%APP_NAME%-ilb
+SET NAFE_LOAD_BALANCER_FRONTEND_IP_NAME=%APP_NAME%-ilb-fip
+SET NAFE_LOAD_BALANCER_POOL_NAME=%APP_NAME%-ilb-pool
+SET NAFE_LOAD_BALANCER_PROBE_PROTOCOL=tcp
+SET NAFE_LOAD_BALANCER_PROBE_INTERVAL=300
+SET NAFE_LOAD_BALANCER_PROBE_COUNT=4
+SET NAFE_LOAD_BALANCER_PROBE_NAME=%NAFE_LOAD_BALANCER_NAME%-probe
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 CALL azure config mode arm
+CALL :CallCLI azure group create --name %RESOURCE_GROUP% --location %LOCATION% --subscription %SUBSCRIPTION%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: create vnet
+CALL :CallCLI azure network vnet create --address-prefixes %VNET_IP_RANGE% --name %VNET_NAME% --location %LOCATION% %POSTFIX%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: create vpn
+CALL :CallCLI azure network vnet subnet create --name GatewaySubnet --vnet-name %VNET_NAME% --address-prefix %GATEWAY_SUBNET_IP_RANGE% %POSTFIX%
+CALL :CallCLI azure network public-ip create --name %VPN_PUBLIC_IP_NAME% --allocation-method Dynamic --location %LOCATION% %POSTFIX%
+CALL :CallCLI azure network vpn-gateway create --name %VPN_GATEWAY_NAME% --vpn-type %VPN_GATEWAY_TYPE% --public-ip-name %VPN_PUBLIC_IP_NAME% --vnet-name %VNET_NAME% --location %LOCATION% %POSTFIX%
+CALL :CallCLI azure network local-gateway create --name %VPN_LOCAL_GATEWAY_NAME% --address-space %ON_PREMISES_ADDRESS_SPACE% --ip-address %ON_PREMISES_PUBLIC_IP% --location %LOCATION% %POSTFIX%
+CALL :CallCLI azure network vpn-connection create --name %VPN_CONNECTION_NAME% --vnet-gateway1 %VPN_GATEWAY_NAME% --vnet-gateway1-group %RESOURCE_GROUP% --lnet-gateway2 %VPN_LOCAL_GATEWAY_NAME% --lnet-gateway2-group %RESOURCE_GROUP% --type IPsec --shared-key %VPN_IPSEC_SHARED_KEY% --location %LOCATION% %POSTFIX%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: create nafe subnet
+CALL :CallCLI azure network vnet subnet create --name %NAFE_SUBNET_NAME% --vnet-name %VNET_NAME% --address-prefix %NAFE_SUBNET_IP_RANGE% %POSTFIX%
+CALL :CallCLI azure network lb create --name %NAFE_LOAD_BALANCER_NAME% --location %LOCATION% %POSTFIX%
+CALL :CallCLI azure network lb frontend-ip create --name %NAFE_LOAD_BALANCER_FRONTEND_IP_NAME% --subnet-vnet-name %VNET_NAME% --subnet-name %NAFE_SUBNET_NAME% --private-ip-address %NAFE_LOAD_BALANCER_FRONTEND_IP_ADDRESS% --lb-name %NAFE_LOAD_BALANCER_NAME% %POSTFIX%
+CALL :CallCLI azure network lb address-pool create --name %NAFE_LOAD_BALANCER_POOL_NAME% --lb-name %NAFE_LOAD_BALANCER_NAME% %POSTFIX%
+CALL :CallCLI azure network lb probe create --name %NAFE_LOAD_BALANCER_PROBE_NAME% --protocol %NAFE_LOAD_BALANCER_PROBE_PROTOCOL% --interval %NAFE_LOAD_BALANCER_PROBE_INTERVAL% --count %NAFE_LOAD_BALANCER_PROBE_COUNT% --lb-name %NAFE_LOAD_BALANCER_NAME% %POSTFIX%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: create nabe subnet
 
-REM Create resources
-
-REM Create the enclosing resource group
-CALL :CallCLI azure group create --name %RESOURCE_GROUP% --location %LOCATION% ^
-  --subscription %SUBSCRIPTION%
-
-REM Create the VNet
-CALL :CallCLI azure network vnet create --address-prefixes %VNET_IP_RANGE% ^
-  --name %VNET_NAME% --location %LOCATION% %POSTFIX%
-
-REM Create the GatewaySubnet
-CALL :CallCLI azure network vnet subnet create --vnet-name %VNET_NAME% ^
-  --address-prefix %GATEWAY_SUBNET_IP_RANGE% --name GatewaySubnet %POSTFIX%
-
-REM Create public IP address for VPN Gateway
-REM Note that the Azure VPN Gateway only supports dynamic IP addresses
-CALL :CallCLI azure network public-ip create --allocation-method Dynamic ^
-  --name %PUBLIC_IP_NAME% --location %LOCATION% %POSTFIX%
-
-REM Create virtual network gateway
-CALL :CallCLI azure network vpn-gateway create --name %VPN_GATEWAY_NAME% ^
-  --vpn-type %VPN_GATEWAY_TYPE% --public-ip-name %PUBLIC_IP_NAME% --vnet-name %VNET_NAME% ^
-  --location %LOCATION% %POSTFIX%
-
-REM Create local gateway
-CALL :CallCLI azure network local-gateway create --name %LOCAL_GATEWAY_NAME% ^
-  --address-space %ON_PREMISES_ADDRESS_SPACE% --ip-address %ON_PREMISES_PUBLIC_IP% ^
-  --location %LOCATION% %POSTFIX%
-
-REM Create a site-to-site connection
-CALL :CallCLI azure network vpn-connection create --name %VPN_CONNECTION_NAME% ^
-  --vnet-gateway1 %VPN_GATEWAY_NAME% --vnet-gateway1-group %RESOURCE_GROUP% ^
-  --lnet-gateway2 %LOCAL_GATEWAY_NAME% --lnet-gateway2-group %RESOURCE_GROUP% ^
-  --type IPsec --location %LOCATION% %POSTFIX%
-
-REM Create the internal subnet
-CALL :CallCLI azure network vnet subnet create --vnet-name %VNET_NAME% ^
-  --address-prefix %INTERNAL_SUBNET_IP_RANGE% --name %INTERNAL_SUBNET_NAME% %POSTFIX%
-
-REM Create an internal load balancer for routing requests
-CALL :CallCLI azure network lb create --name %INTERNAL_LOAD_BALANCER_NAME% ^
-  --location %LOCATION% %POSTFIX%
-
-REM Create a frontend IP address for the internal load balancer
-CALL :CallCLI azure network lb frontend-ip create --subnet-vnet-name %VNET_NAME% ^
-  --subnet-name %INTERNAL_SUBNET_NAME% ^
-  --private-ip-address %INTERNAL_LOAD_BALANCER_FRONTEND_IP_ADDRESS% ^
-  --lb-name %INTERNAL_LOAD_BALANCER_NAME% ^
-  --name %INTERNAL_LOAD_BALANCER_FRONTEND_IP_NAME% ^
-  %POSTFIX%
-
-REM Create the backend address pool for the internal load balancer
-CALL :CallCLI azure network lb address-pool create --lb-name %INTERNAL_LOAD_BALANCER_NAME% ^
-  --name %INTERNAL_LOAD_BALANCER_POOL_NAME% %POSTFIX%
-
-REM Create a health probe for the internal load balancer
-CALL :CallCLI azure network lb probe create --protocol %INTERNAL_LOAD_BALANCER_PROBE_PROTOCOL% ^
-  --interval %INTERNAL_LOAD_BALANCER_PROBE_INTERVAL% --count %INTERNAL_LOAD_BALANCER_PROBE_COUNT% ^
-  --lb-name %INTERNAL_LOAD_BALANCER_NAME% --name %INTERNAL_LOAD_BALANCER_PROBE_NAME% %POSTFIX%
-
-REM This will show the shared key for the VPN connection.  We do not need the error checking.
-CALL azure network vpn-connection shared-key show --name %VPN_CONNECTION_NAME% %POSTFIX%
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 GOTO :eof
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :CallCLI
 SETLOCAL
 CALL %*
@@ -127,7 +73,8 @@ IF ERRORLEVEL 1 (
     (GOTO) 2>NULL & GOTO :eof
 )
 GOTO :eof
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :ShowError
 SETLOCAL EnableDelayedExpansion
 REM Print the message
@@ -145,3 +92,5 @@ GOTO Loop
 :Continue
 ECHO %CLICommand%
 GOTO :eof
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
