42d52654a44b1f30c233e3a10e855170485a2ff6
Hanz Zhang <hanz@microsoft.com>
2016-05-10 15:39:27 -0700

    update szuredeploy.sh

--- ARMBuildingBlocks/guidance-hybrid-network-vpn-advanced/Templates/azuredeploy.sh
+++ ARMBuildingBlocks/guidance-hybrid-network-vpn-advanced/Templates/azuredeploy.sh
@@ -1,3 +1,15 @@
+
+# error handling or interruption via ctrl-c.
+# line number and error code of executed command is passed to errhandle function
+
+trap 'errhandle $LINENO $?' SIGINT ERR
+
+errhandle()
+{
+  echo "Error or Interruption at line ${1} exit code ${2} "
+  exit ${2}
+}
+
 ############################################################################
 
 if [ $# -ne 5 ] 
@@ -23,8 +35,9 @@ azure config mode arm
 RESOURCE_GROUP=${APP_NAME}-ntwk-rg
 vnet6subnetsTemplate=https://raw.githubusercontent.com/mspnp/blueprints/master/ARMBuildingBlocks/ARMBuildingBlocks/Templates/bb-vnet-6subnets.json
 azure group create --name ${RESOURCE_GROUP} --location ${LOCATION} --subscription ${SUBSCRIPTION}
-azure group deployment create --template-uri ${vnet6subnetsTemplate} -g ${RESOURCE_GROUP} -e azuredeploy.param.json 
-##azure group deployment create -f azuredeploy.json -e azuredeploy.param.json -g ${RESOURCE_GROUP}
+parameters="{\"baseName\":{\"value\":\"app2\"},\"onpremNetPrefix\":{\"value\":\"192.168.0.0/24\"},\"vnetPrefix\":{\"value\":\"10.0.0.0/16\"},\"vnetManageSubnetPrefix\":{\"value\":\"10.0.0.0/24\"},\"vnetNvaFeSubnetPrefix\":{\"value\":\"10.0.1.0/24\"},\"vnetNvaBeSubnetPrefix\":{\"value\":\"10.0.2.0/24\"},\"vnetWebSubnetPrefix\":{\"value\":\"10.0.3.0/24\"},\"vnetBizSubnetPrefix\":{\"value\":\"10.0.4.0/24\"},\"vnetDbSubnetPrefix\":{\"value\":\"10.0.5.0/24\"}}"
+azure group deployment create --template-uri ${vnet6subnetsTemplate} -g ${RESOURCE_GROUP} -p ${parameters}
+##azure group deployment create --template-uri ${vnet6subnetsTemplate} -g ${RESOURCE_GROUP} -e azuredeploy.param.json 
 
 ## create web vms and ILB
 ##RESOURCE_GROUP=${APP_NAME}-sn-web-rg
