e47c26024d05395e262621ca7445e4cf341f8f7f
Hanz Zhang <hanz@microsoft.com>
2016-06-03 07:48:58 -0700

    fix IPTAbels sh

--- ARMBuildingBlocks/ARMBuildingBlocks/Scripts/nva.sh
+++ ARMBuildingBlocks/ARMBuildingBlocks/Scripts/nva.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 sudo bash -c "echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf"
 sudo sysctl -p /etc/sysctl.conf
-
-PRIVATE_IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-PUBLIC_IP_ADDRESS=$(wget http://ipinfo.io/ip -qO -)
-
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -X
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.3.254:80
-sudo iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 80 -j SNAT --to-source $PRIVATE_IP_ADDRESS
-sudo iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 80 -j SNAT --to-source $PUBLIC_IP_ADDRESS
-sudo service ufw stop
-sudo service ufw start
