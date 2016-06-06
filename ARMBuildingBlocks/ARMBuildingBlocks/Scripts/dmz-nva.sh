302092c02c334a0bb5d6cfc00cbbddc5c9bf855b
Hanz Zhang <hanz@microsoft.com>
2016-06-06 13:41:18 -0700

    use iptablesiptables-persistent

--- ARMBuildingBlocks/ARMBuildingBlocks/Scripts/dmz-nva.sh
+++ ARMBuildingBlocks/ARMBuildingBlocks/Scripts/dmz-nva.sh
@@ -4,6 +4,7 @@ sysctl -p /etc/sysctl.conf
 
 PRIVATE_IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 PUBLIC_IP_ADDRESS=$(wget http://ipinfo.io/ip -qO -)
+
 iptables -F
 iptables -t nat -F
 iptables -X
@@ -16,18 +17,7 @@ iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 443 -j SNAT --to-sou
 service ufw stop
 service ufw start
 
-echo \#\!/bin/bash > /etc/init.d/iptables.sh
-echo sudo iptables -F >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -F >> /etc/init.d/iptables.sh
-echo sudo iptables -X >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.3.254:80 >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 80 -j SNAT --to-source $PRIVATE_IP_ADDRESS >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 80 -j SNAT --to-source $PUBLIC_IP_ADDRESS >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 10.0.3.254:443 >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 443 -j SNAT --to-source $PRIVATE_IP_ADDRESS >> /etc/init.d/iptables.sh
-echo sudo iptables -t nat -A POSTROUTING -p tcp -d 10.0.3.254 --dport 443 -j SNAT --to-source $PUBLIC_IP_ADDRESS >> /etc/init.d/iptables.sh
-echo sudo service ufw stop >> /etc/init.d/iptables.sh
-echo sudo service ufw start >> /etc/init.d/iptables.sh
+DEBIAN_FRONTEND=noninteractive aptitude install -y -q iptables-persistent
+/etc/init.d/iptables-persistent save
+update-rc.d iptables-persistent defaults
 
-chmod ugo+x /etc/init.d/iptables.sh
-update-rc.d iptables.sh defaults
