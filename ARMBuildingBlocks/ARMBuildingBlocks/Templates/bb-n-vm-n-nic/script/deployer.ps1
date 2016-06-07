0fadea7d7d0f0bad6358d2a9496bdb101de0ba4f
Kirpa Singh <kirpas@microsoft.com>
2016-06-06 17:39:12 -0700

    Updated deployment script

--- ARMBuildingBlocks/ARMBuildingBlocks/Templates/bb-n-vm-n-nic/script/deployer.ps1
+++ ARMBuildingBlocks/ARMBuildingBlocks/Templates/bb-n-vm-n-nic/script/deployer.ps1
@@ -1,31 +1,43 @@
 ﻿Login-AzureRmAccount
-Select-AzureRmSubscription -SubscriptionName 'Pnp Azure'
+Select-AzureRmSubscription -SubscriptionId '6df485a0-aafa-4020-893d-32e833d056d6'
 
 #############################################################################
 # Make sure that the following parameters match the ones in the template
 
-$resourceGroupName='app-6-rg'
+$resourceGroupName='app6-rg'
 $deploymentName='app-6-rg-dep'
 $location='West US'
 
 $vnetName='app6-vnet'
-$subnetName='app6-web-sn'
-$avsetName='app6-web-avSet'
+$subnetName1='app6-fe-subnet'
+$subnetName2='app6-be-subnet'
+#$avsetName='app6-web-avSet'
 
 $vnetAddressPrefix='10.4.0.0/16'
-$subnetAddressPrefix='10.4.0.0/24'
+$subnetAddressPrefix1='10.4.0.0/24'
+$subnetAddressPrefix2='10.4.1.0/24'
+
+#$subnetConfig = @( @{ name='app6-fe-sn'; prefix='10.4.0.0/24' }, @{ name='app6-be-sn'; prefix='10.4.1.0/24' } )
+#for($i=0;$i -lt $subnetConfig.length;$i++)
+#{
+#    $subnet = New-AzureRmVirtualNetworkSubnetConfig -Name $subnetConfig[0].name -AddressPrefix $subnetConfig[0].prefix
+#}
+
+
 
 # Create new resource group
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 
 # Set subnet config
-$subNet=New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
+$subNet1=New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName1 -AddressPrefix $subnetAddressPrefix1
+$subNet2=New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName2 -AddressPrefix $subnetAddressPrefix2
+
 
 # Create a new VNet
-New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName -AddressPrefix $vnetAddressPrefix  -Location $location -Subnet $subNet
+New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName -AddressPrefix $vnetAddressPrefix  -Location $location -Subnet $subNet1, $subNet2
 
 # Create a new availability set
-New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroupName -Name $avsetName -Location $location -PlatformUpdateDomainCount 5 -PlatformFaultDomainCount 3
+#New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroupName -Name $avsetName -Location $location -PlatformUpdateDomainCount 5 -PlatformFaultDomainCount 3
 
 
 # Template and first parameter file URIs
