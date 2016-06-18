0db7afc2bcdb49782e25453ef072bc2fc523fc23
Kirpa Singh <kirpas@microsoft.com>
2016-06-18 12:52:51 -0700

    Changed deployment name to by dynamic to avoid conflict

--- ARMBuildingBlocks/ARMBuildingBlocks/Templates/buildingBlocks/Multi-vm-n-nic-m-storage/script/deployer.ps1
+++ ARMBuildingBlocks/ARMBuildingBlocks/Templates/buildingBlocks/Multi-vm-n-nic-m-storage/script/deployer.ps1
@@ -4,26 +4,18 @@ Select-AzureRmSubscription -SubscriptionId '6df485a0-aafa-4020-893d-32e833d056d6
 #############################################################################
 # Make sure that the following parameters match the ones in the template
 
-$resourceGroupName='app6-rg'
-$deploymentName='app-6-rg-dep'
+$resourceGroupName='app13-rg'
+$deploymentName='app13-rg-dep'
 $location='West US'
 
-$vnetName='app6-vnet'
-$subnetName1='app6-fe-subnet'
-$subnetName2='app6-be-subnet'
-#$avsetName='app6-web-avSet'
+$vnetName='app13-vnet'
+$subnetName1='app13-fe-subnet'
+$subnetName2='app13-be-subnet'
+$avsetName='app13-web-avSet'
 
 $vnetAddressPrefix='10.4.0.0/16'
 $subnetAddressPrefix1='10.4.0.0/24'
 $subnetAddressPrefix2='10.4.1.0/24'
-
-#$subnetConfig = @( @{ name='app6-fe-sn'; prefix='10.4.0.0/24' }, @{ name='app6-be-sn'; prefix='10.4.1.0/24' } )
-#for($i=0;$i -lt $subnetConfig.length;$i++)
-#{
-#    $subnet = New-AzureRmVirtualNetworkSubnetConfig -Name $subnetConfig[0].name -AddressPrefix $subnetConfig[0].prefix
-#}
-
-
 
 # Create new resource group
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
@@ -37,7 +29,7 @@ $subNet2=New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName2 -AddressPrefix
 New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName -AddressPrefix $vnetAddressPrefix  -Location $location -Subnet $subNet1, $subNet2
 
 # Create a new availability set
-#New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroupName -Name $avsetName -Location $location -PlatformUpdateDomainCount 5 -PlatformFaultDomainCount 3
+New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroupName -Name $avsetName -Location $location -PlatformUpdateDomainCount 5 -PlatformFaultDomainCount 3
 
 
 # Template and first parameter file URIs
