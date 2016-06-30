9ac9540e3bf07ae553a6823934d4b5dbc06ed600
Kirpa Singh <kirpas@microsoft.com>
2016-06-30 13:19:47 -0700

    PArameters

--- ARMBuildingBlocks/ARMBuildingBlocks/Drivers/Powershell/deploy-vnet-n-subnets.ps1
+++ ARMBuildingBlocks/ARMBuildingBlocks/Drivers/Powershell/deploy-vnet-n-subnets.ps1
@@ -17,12 +17,14 @@ $location='West US'
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
 
 # Template and parameters file URIs
-$templateUri = 'https://raw.githubusercontent.com/mspnp/blueprints/refarch/buildingblocks/ARMBuildingBlocks/ARMBuildingBlocks/Templates/buildingBlocks/vnet-n-subnet/azuredeploy.json'
-$templateParamUri = 'https://raw.githubusercontent.com/mspnp/blueprints/refarch/buildingblocks/ARMBuildingBlocks/ARMBuildingBlocks/Templates/buildingBlocks/vnet-n-subnet/vnet-with-3tier-subnets.parameters.json'
+$templateUri = 'https://raw.githubusercontent.com/mspnp/blueprints/kirpas/buildingblocks/ARMBuildingBlocks/ARMBuildingBlocks/Templates/buildingBlocks/vnet-n-subnet/azuredeploy.json'
+$templateParamUri='https://raw.githubusercontent.com/mspnp/blueprints/kirpas/buildingblocks/ARMBuildingBlocks/ARMBuildingBlocks/Templates/buildingBlocks/vnet-n-subnet/scenarios/azuredeploy.parameters.json'
 
-$result = Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterFile $templateParamUri
+$result = Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterUri $templateParamUri
 
 # Test-AzureRmResourceGroupDeployment returns a list of PSResourceManagerError objects, so a count of 0 is all clear signal!
 if($result.Count -eq 0){
-	New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterFile $templateParamUri -Verbose
+	New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterUri $templateParamUri -Verbose
+}else{
+    $result
 }
