b49123ee4103b4e13a1dfa29cf5f872fac34bd9b
Hanz Zhang <hanz@microsoft.com>
2016-07-19 09:11:00 -0700

    refactor comments in azuredeploy.sh for adfs

--- ARMBuildingBlocks/guidance-iaas-ra-adfs/Templates/azuredeploy.sh
+++ ARMBuildingBlocks/guidance-iaas-ra-adfs/Templates/azuredeploy.sh
@@ -1,7 +1,13 @@
 ############################################################################
-##  Arguments
 ############################################################################
-# Please fill in your data here ...
+############################################################################
+############################################################################
+##  Configurations
+############################################################################
+
+############################################################################
+## You must fill in the following configuration data
+############################################################################
 BASE_NAME=
 SUBSCRIPTION=
 LOCATION=
@@ -15,30 +21,39 @@ VPN_IPSEC_SHARED_KEY=
 ON_PREMISES_DNS_SERVER_ADDRESS=
 ON_PREMISES_DNS_SUBNET_PREFIX=
 ############################################################################
+
+############################################################################
+## Other configuation data
+############################################################################
+
+# step-by-step prompt for this script, set it to true will allow you 
+# to check and verify each step before going to the next step. 
 Prompting=true
 
 # location of arm templates
 URI_BASE=https://raw.githubusercontent.com/mspnp/blueprints/master/ARMBuildingBlocks
 
-# Active directory replication frequency in minutes
-REPLICATION_FREQUENCY=15
-SITE_NAME=Azure-Vnet-Ad-Site
+############################################################################
+# Active directory site name in on-premises network
 ONPREM_SITE_NAME=Default-First-Site-Name
+# Active directory replication site name in azure vNet
+SITE_NAME=Azure-Vnet-Ad-Site
+# Active directry site replication frequency in minutes
+REPLICATION_FREQUENCY=15
 
-# Active directory replication frequency in minutes
+############################################################################
+# DSC type handler version
 DSC_TYPE_HANDLER_VERSION=2.19
 
-NTWK_RESOURCE_GROUP=${BASE_NAME}-ntwk-rg
+############################################################################
+# vNet and subnet address prefix
 
-# VNet parameter defaults
 VNET_PREFIX=10.0.0.0/16
 
 VNET_NVA_FE_SUBNET_PREFIX=10.0.0.0/27
 VNET_NVA_BE_SUBNET_PREFIX=10.0.0.32/27
-
 VNET_DMZ_FE_SUBNET_PREFIX=10.0.0.64/27
 VNET_DMZ_BE_SUBNET_PREFIX=10.0.0.96/27
-
 VNET_MGMT_SUBNET_PREFIX=10.0.0.128/25
 
 VNET_WEB_SUBNET_PREFIX=10.0.1.0/24
@@ -47,28 +62,14 @@ VNET_DB_SUBNET_PREFIX=10.0.3.0/24
 
 VNET_GATEWAY_SUBNET_ADDRESS_PREFIX=10.0.255.224/27
 VNET_AD_SUBNET_PREFIX=10.0.255.192/27
-
 VNET_ADFS_SUBNET_PREFIX=10.0.255.160/27
 VNET_ADFS_PROXY_SUBNET_PREFIX=10.0.255.128/27
 
-
-# the following variables are used in the creation of vpn, web/biz/db tier, but not using in vnet creation
-AD_SERVER_IP_ADDRESS_ARRAY=[\"10.0.255.222\",\"10.0.255.221\"]
-DNS_SERVER_ADDRESS_ARRAY=[\"10.0.255.222\",\"10.0.255.221\",\"${ON_PREMISES_DNS_SERVER_ADDRESS}\"]
-
-MGMT_JUMPBOX_IP_ADDRESS=10.0.0.254
-NVA_FE_ILB_IP_ADDRESS=10.0.0.30
-WEB_ILB_IP_ADDRESS=10.0.1.254
-BIZ_ILB_IP_ADDRESS=10.0.2.254
-DB_ILB_IP_ADDRESS=10.0.3.254
-
-NVA_FE_VM_IP_ADDRESSES=[\"10.0.0.29\",\"10.0.0.28\"]
-NVA_BE_VM_IP_ADDRESSES=[\"10.0.0.62\",\"10.0.0.61\"]
-NVA_MGMT_VM_IP_ADDRESSES=[\"10.0.0.253\",\"10.0.0.252\"]
-
-DMZ_FE_VM_IP_ADDRESSES=[\"10.0.0.94\",\"10.0.0.93\"]
-DMZ_BE_VM_IP_ADDRESSES=[\"10.0.0.126\",\"10.0.0.125\"]
-DMZ_MGMT_VM_IP_ADDRESSES=[\"10.0.0.251\",\"10.0.0.250\"]
+############################################################################
+# static private address for ILBs and Servers
+AD_SERVER_IP_ADDRESSES=\"10.0.255.222\",\"10.0.255.221\"
+AD_SERVER_IP_ADDRESS_ARRAY=[${AD_SERVER_IP_ADDRESSES}]
+DNS_SERVER_ADDRESS_ARRAY=[${AD_SERVER_IP_ADDRESSES},\"${ON_PREMISES_DNS_SERVER_ADDRESS}\"]
 
 ADFS_ILB_IP_ADDRESS=10.0.255.190
 ADFS_SERVER_IP_ADDRESS_ARRAY=[\"10.0.255.189\",\"10.0.255.188\"]
@@ -76,6 +77,26 @@ ADFS_SERVER_IP_ADDRESS_ARRAY=[\"10.0.255.189\",\"10.0.255.188\"]
 ADFS_PROXY_ILB_IP_ADDRESS=10.0.255.158
 ADFS_PROXY_SERVER_IP_ADDRESS_ARRAY=[\"10.0.255.157\",\"10.0.255.156\"]
 
+MGMT_JUMPBOX_IP_ADDRESS=10.0.0.254
+NVA_MGMT_VM_IP_ADDRESSES=[\"10.0.0.253\",\"10.0.0.252\"]
+DMZ_MGMT_VM_IP_ADDRESSES=[\"10.0.0.251\",\"10.0.0.250\"]
+
+DMZ_BE_VM_IP_ADDRESSES=[\"10.0.0.126\",\"10.0.0.125\"]
+DMZ_FE_VM_IP_ADDRESSES=[\"10.0.0.94\",\"10.0.0.93\"]
+
+NVA_BE_VM_IP_ADDRESSES=[\"10.0.0.62\",\"10.0.0.61\"]
+
+NVA_FE_ILB_IP_ADDRESS=10.0.0.30
+NVA_FE_VM_IP_ADDRESSES=[\"10.0.0.29\",\"10.0.0.28\"]
+
+WEB_ILB_IP_ADDRESS=10.0.1.254
+
+BIZ_ILB_IP_ADDRESS=10.0.2.254
+
+DB_ILB_IP_ADDRESS=10.0.3.254
+
+############################################################################
+# Number of VMs in each tier
 WEB_NUMBER_VMS=2
 BIZ_NUMBER_VMS=2
 DB_NUMBER_VMS=2
@@ -83,6 +104,13 @@ AD_NUMBER_VMS=2
 ADFS_NUMBER_VMS=2
 ADFS_PROXY_NUMBER_VMS=2
 
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+# Error Handling
+############################################################################
 
 ############################################################################
 # error handling or interruption via ctrl-c.
@@ -128,17 +156,29 @@ then
   exit
 fi
 ############################################################################
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+
+############################################################################
+# Set azure CLI to arm mode
+############################################################################
 
 echo
 echo
 echo azure config mode arm
      azure config mode arm
+	 
 ############################################################################
-## Create vNet and Subnets for mgmt, nva-fe, nva-be, web, biz, db
+############################################################################
+############################################################################
+############################################################################
+## Create vNet (my-ntwk-rg)
 ############################################################################
 
 TEMPLATE_URI=${URI_BASE}/guidance-iaas-ra-adfs/Templates/ra-adfs/azuredeploy.json
-
+NTWK_RESOURCE_GROUP=${BASE_NAME}-ntwk-rg
 RESOURCE_GROUP=${NTWK_RESOURCE_GROUP}
 ON_PREM_NET_PREFIX=${ON_PREMISES_ADDRESS_SPACE}
 DNS_SERVERS=[]
@@ -184,9 +224,14 @@ if [ "${Prompting}" == "true" ]; then
 	read -p "Press any key to continue ... " -n1 -s
 fi
 ############################################################################
-## Create ILB and VMs in web, biz, db
 ############################################################################
-# create web tier
+############################################################################
+############################################################################
+## Create Web Tiere (my-web-tier-rg)
+############################################################################
+
+############################################################################
+# create web tier (ILB and VMs)
 TEMPLATE_URI=${URI_BASE}/ARMBuildingBlocks/Templates/bb-ilb-backend-http-https.json
 SUBNET_NAME_PREFIX=${DEPLOYED_WEB_SUBNET_NAME_PREFIX}
 ILB_IP_ADDRESS=${WEB_ILB_IP_ADDRESS}
@@ -209,6 +254,7 @@ echo
 echo azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
      azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
 
+############################################################################
 # install iis/apache to web vms
 for (( i=1; i<=${NUMBER_VMS}; i++ ))
 do
@@ -241,7 +287,13 @@ if [ "${Prompting}" == "true" ]; then
 	read -p "Press any key to continue ... " -n1 -s
 fi
 
-#### # create biz tier
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+#### # Create biz tier (my-biz-tier-rg)
+############################################################################
+
 #### TEMPLATE_URI=${URI_BASE}/ARMBuildingBlocks/Templates/bb-ilb-backend-http-https.json
 #### SUBNET_NAME_PREFIX=${DEPLOYED_BIZ_SUBNET_NAME_PREFIX}
 #### ILB_IP_ADDRESS=${BIZ_ILB_IP_ADDRESS}
@@ -264,7 +316,15 @@ fi
 #### echo azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
 ####      azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
 #### 
-#### # create db tier
+
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+#### # create db tier (my-db-tier-rg)
+############################################################################
+
 #### TEMPLATE_URI=${URI_BASE}/ARMBuildingBlocks/Templates/bb-ilb-backend-http-https.json
 #### SUBNET_NAME_PREFIX=${DEPLOYED_DB_SUBNET_NAME_PREFIX}
 #### ILB_IP_ADDRESS=${DB_ILB_IP_ADDRESS}
@@ -286,7 +346,15 @@ fi
 #### echo
 #### echo azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
 ####      azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
+
  
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+## Create Management Resources (my-mgmt-rg)
+############################################################################
+
 ############################################################################
 ## Create ILB and NVA VMs in nva subnet
 ############################################################################
@@ -359,6 +427,13 @@ if [ "${Prompting}" == "true" ]; then
 fi
 
 ############################################################################
+############################################################################
+############################################################################
+############################################################################
+## Create VPN (my-ntwk-rg)
+############################################################################
+
+############################################################################
 ## Create VPN Gateway and VPN connection to connect to on premises network
 ############################################################################
 TEMPLATE_URI=${URI_BASE}/ARMBuildingBlocks/Templates/bb-vpn-gateway-connection.json
@@ -386,6 +461,13 @@ if [ "${Prompting}" == "true" ]; then
 	echo
 	read -p "Press any key to continue ... " -n1 -s
 fi
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+## Create DMZ (my-dmz-rg)
+############################################################################
 
 ############################################################################
 ## Create Public LB and NVA Vms in dmz subnet 
@@ -424,6 +506,13 @@ if [ "${Prompting}" == "true" ]; then
 fi
 
 ############################################################################
+############################################################################
+############################################################################
+############################################################################
+## Connect to On-premises Network
+############################################################################
+
+############################################################################
 ## Manual Step: config on premise router to make on-prem-to-azure connection
 ############################################################################
 #  if [ "${Prompting}" == "true" ]; then
@@ -438,6 +527,7 @@ fi
 	echo
 	read -p "Press any key to continue ... " -n1 -s
 #  fi
+
 ############################################################################
 ## Update vNet DNS setting to on-premises DNS
 ############################################################################
@@ -456,7 +546,15 @@ echo
 echo azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
      azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
 
-## Create dns resource group
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+## Create ADDS/DNS servers (my-dns-rg)
+############################################################################
+
+############################################################################
+## Create adds/dns resource group
 ############################################################################
 
 if [ "${Prompting}" == "true" ]; then
@@ -476,7 +574,8 @@ echo
 echo azure group create --name ${RESOURCE_GROUP} --location ${LOCATION} --subscription ${SUBSCRIPTION}
      azure group create --name ${RESOURCE_GROUP} --location ${LOCATION} --subscription ${SUBSCRIPTION}
 
-## Create dns vms
+############################################################################
+## Create adds/dns vms
 ############################################################################
 if [ "${Prompting}" == "true" ]; then
 	echo
@@ -497,8 +596,10 @@ echo
 echo
 echo azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
      azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
+	 
 ############################################################################
-# Join the VMs to On-Prem AD Domain
+# Join the adds VMs to On-Prem AD Domain
+############################################################################
 if [ "${Prompting}" == "true" ]; then
 	echo
 	echo
@@ -528,6 +629,7 @@ fi
  
 ############################################################################
 # install ADDS replication site 
+############################################################################
 if [ "${Prompting}" == "true" ]; then
 	echo
 	echo
@@ -548,8 +650,10 @@ if [ "${Prompting}" == "true" ]; then
 	echo
 	read -p "Press any key to continue ... " -n1 -s
 fi
+
 ############################################################################
-# install ADDS/DNS to all DNS servers
+# install ADDS/DNS to all adds/dns servers
+############################################################################
 if [ "${Prompting}" == "true" ]; then
 	echo
 	echo
@@ -577,6 +681,7 @@ if [ "${Prompting}" == "true" ]; then
 	echo
 	read -p "Press any key to continue ... " -n1 -s
 fi
+
 ############################################################################
 ## Update vNet DNS setting to the Azure AD Servers
 ############################################################################
@@ -629,6 +734,12 @@ echo azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_
      azure group deployment create --template-uri ${TEMPLATE_URI} -g ${RESOURCE_GROUP} -p ${PARAMETERS} --subscription ${SUBSCRIPTION}
 
 ############################################################################
+############################################################################
+############################################################################
+############################################################################
+## Enable Forced Tunneling
+############################################################################
+
 ## UnComment the following lines to enable forced tunneling in web/biz/db tier
 #TEMPLATE_URI=${URI_BASE}/ARMBuildingBlocks/Templates/bb-ntwk-forced-tunneling.json
 #RESOURCE_GROUP=${NTWK_RESOURCE_GROUP}
