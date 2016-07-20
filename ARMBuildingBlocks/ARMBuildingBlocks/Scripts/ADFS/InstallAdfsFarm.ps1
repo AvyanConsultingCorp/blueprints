03869711b9baf920c300dbd61c797a64b73699af
Hanz Zhang <hanz@microsoft.com>
2016-07-20 14:59:10 -0700

    update InstallAdfsFarm.ps1

--- ARMBuildingBlocks/ARMBuildingBlocks/Scripts/ADFS/InstallAdfsFarm.ps1
+++ ARMBuildingBlocks/ARMBuildingBlocks/Scripts/ADFS/InstallAdfsFarm.ps1
@@ -51,6 +51,8 @@
 #	 MyFakeRootCertificateAuthority.pvk
 #    mytestcertificate.pfx
 
+###################
+
 # 7. RDP to the each ADFS VM.
 
 # 8. copy 
@@ -89,45 +91,39 @@
 #      \Certificates (Local Computer)\Personal\Certificates\myadfs.contoso.com issued by A Real Certification Authority
 
 ###############################################
-
-# retrieve the the thumbnail of certificate
-$thumbprint=(Get-ChildItem -DnsName $FederationName -Path cert:\LocalMachine\My).Thumbprint
-
 # domainjoin script needs to be executed first
 # example of command
 #.\AddAdfsFarm.ps1 -AdminUser "domainuser" -AdminPassword "domainPass" -NetBiosDomainName "patterns2" -FqDomainName "patternspractices.net" -GmsaAdfs "adfsacct" -FederationName "pnpadfs.patternspractices.net" -descriptionAdfs "PNP ADFS"
 
+$AdminUser = "adminUser"
+$AdminPassword = "adminP@ssw0rd"
+$NetBiosDomainName = "CONTOSO"
+$FqDomainName = "contoso.com"
+$GmsaAdfs = "adfsaccount"
+$FederationName = "myadsf.contoso.com"
+$DescriptionAdfs = "Contoso ADSF"
+
 Install-WindowsFeature -IncludeManagementTools -Name ADFS-Federation
+
 Import-Module ADFS
 
 $secAdminPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+
 $credential = New-Object System.Management.Automation.PSCredential ("$NetBiosDomainName\$AdminUser", $secAdminPassword)
 
-#----GMSA under which ADFS service runs under-------------------------------------
+# retrieve the the thumbnail of certificate
+$thumbprint=(Get-ChildItem -DnsName $FederationName -Path cert:\LocalMachine\My).Thumbprint
 
 Add-KdsRootKey –EffectiveTime (Get-Date).AddHours(-10) 
 
+# GMSA under which ADFS service runs under
 New-ADServiceAccount $GmsaAdfs -DNSHostName "$GmsaAdfs.$FqDomainName" -AccountExpirationDate $null -ServicePrincipalNames "http://$GmsaAdfs.$FqDomainName" -Credential $credential
 
-#------------------------------------------------------------------------------
-
-
 # here if we want to test the deployment is a good practice
-
-#Test-AdfsFarmInstallation -CertificateThumbprint $thumbprint -FederationServiceDisplayName $federationName -FederationServiceName $federationName -GroupServiceAccountIdentifier "patterns\pnpacct`$" -Credential $credential
-
 #Write-Host "$NetBiosDomainName\$GmsaAdfs`$"
+#Test-AdfsFarmInstallation Install-AdfsFarm  -CertificateThumbprint $thumbprint -FederationServiceDisplayName $DescriptionAdfs -FederationServiceName $federationName -GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaAdfs`$" -Credential $credential
 
 Install-AdfsFarm  -CertificateThumbprint $thumbprint -FederationServiceDisplayName $DescriptionAdfs -FederationServiceName $FederationName -GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaAdfs`$" -Credential $credential -OverwriteConfiguration
-
-
-#Install-AdfsFarm  -CertificateThumbprint $thumbprint `
-#-FederationServiceDisplayName $descriptionAdfs `
-#-FederationServiceName $FederationName ` 
-#-GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaAdfs`$" `
-#-Credential $credential -OverwriteConfiguration
-
-#------------------------------------------------------------------------------
 
 # device registration service for workplace join 
 Initialize-ADDeviceRegistration -ServiceAccountName "$NetBiosDomainName\$GmsaAdfs`$" `
