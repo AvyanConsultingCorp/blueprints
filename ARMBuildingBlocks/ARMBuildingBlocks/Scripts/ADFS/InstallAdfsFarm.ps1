42119b0fba63614199afeb79ca01152a26fbe56b
Hanz Zhang <hanz@microsoft.com>
2016-07-22 18:56:36 -0700

    add NewAdserviceAccount

--- ARMBuildingBlocks/ARMBuildingBlocks/Scripts/ADFS/InstallAdfsFarm.ps1
+++ ARMBuildingBlocks/ARMBuildingBlocks/Scripts/ADFS/InstallAdfsFarm.ps1
@@ -12,7 +12,7 @@
   [string]$FqDomainName,
 
   [Parameter(Mandatory=$True)]
-  [string]$GmsaAdfs,
+  [string]$GmsaName,
 
   [Parameter(Mandatory=$True)]
   [string]$FederationName,
@@ -38,27 +38,20 @@
 #	 C:/temp/MyFakeRootCertificateAuthority.pvk
 
 # 4. Run command prompt as admin to use my fake root certificate authority to generate
-#    a certificate for myadfs.contoso.com and enterpriseregistration.contoso.com
+#    a certificate for adfs.contoso.com
 #
-# makecert -sk pkey -iv MyFakeRootCertificateAuthority.pvk -a sha256 -n "CN=*.contoso.com" -ic MyFakeRootCertificateAuthority.cer -sr localmachine -ss my -sky exchange -pe
-# makecert -sk pkey -iv MyFakeRootCertificateAuthority.pvk -a sha256 -n "CN=adfs1.contoso.com , CN=enterpriseregistration.contoso.com" -ic MyFakeRootCertificateAuthority.cer -sr localmachine -ss my -sky exchange -pe
 # makecert -sk pkey -iv MyFakeRootCertificateAuthority.pvk -a sha256 -n "CN=adfs.contoso.com , CN=enterpriseregistration.contoso.com" -ic MyFakeRootCertificateAuthority.cer -sr localmachine -ss my -sky exchange -pe
 
 # 5. start mmc certificates console 
-#	Expand to /Certificates (Local Computer)/Personal/Certificate/*.contoso.com 
-#	Expand to /Certificates (Local Computer)/Personal/Certificate/adfs1.contoso.com 
+#	Expand to /Certificates (Local Computer)/Personal/Certificate/adfs.contoso.com 
 #
 #	Export the certificate with the private key to 
-#    C:/temp/contosocom.pfx
 #    C:/temp/adfscontosocom.pfx
-#    C:/temp/adfs1contosocom.pfx
 
 # 6. Make sure you have the following files in the C:\temp
 #	 MyFakeRootCertificateAuthority.cer
 #	 MyFakeRootCertificateAuthority.pvk
-#    contosocom.pfx
 #    adfscontosocom.pfx
-#    adfs1contosocom.pfx
 
 ###################
 
@@ -67,9 +60,7 @@
 # 8. copy 
 #		certutil.exe
 #		MyFakeRootCertificateAuthority.cer
-#       contosocom.pfx
 #       adfscontosocom.pfx
-#       adfs1contosocom.pfx
 #    to 
 #		C:\temp\ 
 
@@ -79,13 +70,9 @@
 #      \Certificates (Local Computer)\Trusted Root Certification Authorities\Certificates\MyFakeRootCertificateAuthority 
 
 # 10. Run the following command prompt as admin:
-#  		certutil.exe -privatekey -importPFX my C:\temp\contosocom.pfx NoExport
 #  		certutil.exe -privatekey -importPFX my C:\temp\adfscontosocom.pfx NoExport
-#  		certutil.exe -privatekey -importPFX my C:\temp\adfs1contosocom.pfx NoExport
 #   Open mmc Certificate Console and verify that it now has the following item
-#      \Certificates (Local Computer)\Personal\Certificates\*.contoso.com issued by MyFakeRootCertificationAuthority 
 #      \Certificates (Local Computer)\Personal\Certificates\adfs.contoso.com issued by MyFakeRootCertificationAuthority 
-#      \Certificates (Local Computer)\Personal\Certificates\adfs1.contoso.com issued by MyFakeRootCertificationAuthority 
 
 # 11. Repeat step 7 to 10 for next ADDS server
 
@@ -101,7 +88,7 @@
 #		C:\temp\ 
 
 # 10. Run the following command prompt as admin:
-#    	certutil.exe -privatekey -importPFX my C:\temp\adfs.contoso.com.pfx NoExport
+#    	certutil.exe -privatekey -importPFX my C:\temp\contoso.com.pfx NoExport
 #   Open mmc Certificate Console and verify that it now has the following item
 #      \Certificates (Local Computer)\Personal\Certificates\adfs.contoso.com issued by A Real Certification Authority
 
@@ -110,7 +97,7 @@
 # $AdminPassword = "adminP@ssw0rd"
 # $NetBiosDomainName = "CONTOSO"
 # $FqDomainName = "contoso.com"
-# $GmsaAdfs = "adfsservice"
+# $GmsaName = "adfsgmsa"
 # $FederationName = "adfs.contoso.com"
 # $DescriptionAdfs = "Contoso Corporation"
 
@@ -118,27 +105,21 @@
 # domainjoin script needs to be executed first
 
 # retrieve the the thumbnail of certificate
-#$thumbprint=(Get-ChildItem -DnsName *.$FqDomainName -Path cert:\LocalMachine\My).Thumbprint
 $thumbprint=(Get-ChildItem -DnsName $FederationName -Path cert:\LocalMachine\My).Thumbprint
 Write-Host $thumbprint
 
 $secAdminPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ("$NetBiosDomainName\$AdminUser", $secAdminPassword)
 
-# Create GMSA under which ADFS service runs under
-Install-windowsfeature -name AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
-Import-Module ADDSDeployment
-New-ADServiceAccount $GmsaAdfs -DNSHostName "$GmsaAdfs.$FqDomainName" -AccountExpirationDate $null -ServicePrincipalNames "http://$GmsaAdfs.$FqDomainName" -Credential $credential
-setspn -a host/$FederationName $GmsaAdfs
-
 # Install ADFS feature
 Install-WindowsFeature -IncludeManagementTools -Name ADFS-Federation
 Import-Module ADFS
 Add-KdsRootKey –EffectiveTime (Get-Date).AddHours(-10) 
-Install-AdfsFarm  -CertificateThumbprint $thumbprint -FederationServiceDisplayName $DescriptionAdfs -FederationServiceName $FederationName -GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaAdfs`$" -Credential $credential -OverwriteConfiguration
+Install-AdfsFarm -CertificateThumbprint $thumbprint -FederationServiceDisplayName $DescriptionAdfs -FederationServiceName $FederationName -GroupServiceAccountIdentifier "$NetBiosDomainName\$GmsaName`$" -Credential $credential -OverwriteConfiguration
 
 # device registration service for workplace join 
-Initialize-ADDeviceRegistration -ServiceAccountName "$NetBiosDomainName\$GmsaAdfs`$" -DeviceLocation $FqDomainName -RegistrationQuota 10 -MaximumRegistrationInactivityPeriod 90 -Credential $Credential -Force
+Initialize-ADDeviceRegistration -ServiceAccountName "$NetBiosDomainName\$GmsaName`$" -DeviceLocation $FqDomainName -RegistrationQuota 10 -MaximumRegistrationInactivityPeriod 90 -Credential $Credential -Force
 Enable-AdfsDeviceRegistration -Credential $Credential -Force
 
-Restart-Computer
+# to test, browse to 
+# https://adfs.contoso.com/adfs/ls/idpinitiatedsignon.htm
---
+++ ARMBuildingBlocks/ARMBuildingBlocks/Scripts/ADFS/NewAdserviceAccount.ps1
@@ -0,0 +1,29 @@
+﻿Param(
+  [Parameter(Mandatory=$True)]
+  [string]$AdminUser,
+
+  [Parameter(Mandatory=$True)]
+  [string]$AdminPassword,
+
+  [Parameter(Mandatory=$True)]
+  [string]$NetBiosDomainName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$GmsaName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$FederationName
+
+)
+
+# $AdminUser = "adminUser"
+# $AdminPassword = "adminP@ssw0rd"
+# $NetBiosDomainName = "CONTOSO"
+# $GmsaName = "adfsgmsa"
+# $FederationName = "adfs.contoso.com"
+
+$secAdminPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("$NetBiosDomainName\$AdminUser", $secAdminPassword)
+Add-KdsRootKey –EffectiveTime (Get-Date).AddHours(-10) 
+#New-ADServiceAccount adfsgmsa -DNSHostName adfs.contoso.com -AccountExpirationDate $null -ServicePrincipalNames host/adfs.contoso.com -Credential $credential
+New-ADServiceAccount $GmsaName -DNSHostName $FederationName -AccountExpirationDate $null -ServicePrincipalNames "host/$FederationName" -Credential $credential
