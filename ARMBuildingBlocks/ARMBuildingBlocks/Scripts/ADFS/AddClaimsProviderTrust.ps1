85d0a67986be5d4fcf194014fd5df0fd9d8e34ac
cfarre <cfarre@microsoft.com>
2016-08-03 11:19:33 -0700

    claims provider trust

---
+++ ARMBuildingBlocks/ARMBuildingBlocks/Scripts/ADFS/AddClaimsProviderTrust.ps1
@@ -0,0 +1,32 @@
+﻿Param(
+  [Parameter(Mandatory=$True)]
+  [string]$ClaimsProviderName,
+
+  [Parameter(Mandatory=$True)]
+  [string]$MetadataUrl,
+
+  [Parameter(Mandatory=$True)]
+  [string]$TransformRule
+
+)
+
+
+# this script will add the claims provider on Resource ADFS
+# it must be ran on Resource ADFS 
+# it is high privilege operation and needs to be ran as domain admin
+# the metadata must point to Account ADFS
+
+# example
+
+# ./AddClaimsProviderTrust -ClaimsProviderName 'Resource Account Adfs' -MetadataUrl 'https://pnpadfs.patternspractices.com/FederationMetadata/2007-06/FederationMetadata.xml' -TransformRule ‘c:[Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"] => issue(claim = c);’
+  
+   
+  Add-AdfsClaimsProviderTrust -Name $ClaimsProviderName `
+  -AutoUpdateEnabled $true `
+   -MetadataUrl $MetadataUrl  `
+   -AcceptanceTransformRules $TransformRule  
+
+
+
+
+
